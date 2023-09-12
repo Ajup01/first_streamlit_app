e56a81f37a769b22740741b6b8e6c050b49a03e1
Ajup01 <134443368+Ajup01@users.noreply.github.com>
2023-09-12 15:57:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
+
+import snowflake.connector
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
